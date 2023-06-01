3bf78b5241641cdd93da690392b1e2e6c35f0f69
“NatiRollan” <natyangela2010@gmail.com>
2023-06-01 12:42:13 -0300

    Ejercicio 3

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,35 +1,88 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*Console.WriteLine("Hello, World!");
 
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);*/
+int respuesta;
 
-int intNum1=0;
-string? numString= "";
-Console.WriteLine("Ingrese un numero: ");
-numString = Console.ReadLine();
-if (int.TryParse(numString, out intNum1))
+do
 {
-    if (intNum1 > 0)
+    Console.WriteLine("------------- MENU ------------------");
+    Console.WriteLine("1- Sumar");
+    Console.WriteLine("2- Restar");
+    Console.WriteLine("3- Multiplicar");
+    Console.WriteLine("4- Dividir");
+    Console.WriteLine("5- Otras operaciones");
+    Console.WriteLine("Seleccione una opcion: ");
+
+    int opcion;
+    int.TryParse(Console.ReadLine(), out opcion); //lee un string ingresado por el usuario, convierte a nro entero y almacena el resultado en la variable opcion
+
+    float num, num1, num2;
+    float resultado = 0; //en c# cuando divido 2 nros enteros el resultado se trunca automatica_ a nro entero
+                         //si quiero un resultado con decimales, entonces num1 y num2 tienen que ser float
+    
+    if (opcion == 5)
     {
-        string? numAux = "";
-        while (intNum1 != 0)
+        Console.WriteLine("Ingrese un numero: ");
+        float.TryParse(Console.ReadLine(), out num);
+
+        Console.WriteLine("Numero: " + num);
+        Console.WriteLine("Valor Absoluto: " + Math.Abs(num));
+        Console.WriteLine("Cuadradro: " + Math.Pow(num, 2));
+        if (num >= 0)
         {
-            int digito = intNum1 % 10;
-            numAux = numAux + digito.ToString();
-            intNum1 /= 10;
+            Console.WriteLine("Raiz cuadrada: " + Math.Sqrt(num));
+        }else
+        {
+            Console.WriteLine("Raiz cuadrada: No se puede obtener");
         }
-        Console.WriteLine("El numero invertido es: " + numAux);
+        Console.WriteLine("Seno: " + (float)Math.Sin(num));
+        Console.WriteLine("Coseno: " + (float)Math.Cos(num));
+        Console.WriteLine("Parte entera de un tipo float: " + (int)num);
     } else
     {
-        Console.WriteLine("El texto ingresado no es mayor a 0");
-    }
+        Console.WriteLine("Ingrese el primer numero: ");
+        float.TryParse(Console.ReadLine(), out num1);
+        Console.WriteLine("Ingrese el segundo numero: ");
+        float.TryParse(Console.ReadLine(), out num2);
 
-} else
-{
-    Console.WriteLine("El texto ingresado no es un numero");
-}
+        switch (opcion)
+        {
+            case 1:
+                resultado = num1 + num2;
+                Console.WriteLine("El resultado es: " + resultado);
+            break;
+            case 2:
+                resultado = num1 - num2;
+                Console.WriteLine("El resultado es: " + resultado);
+            break;
+            case 3:
+                resultado = num1 * num2;
+                Console.WriteLine("El resultado es: " + resultado);
+            break;
+            case 4:
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                    Console.WriteLine("El resultado es: " + resultado);
+                } else
+                {
+                    Console.WriteLine("Error");
+                }
+            break;   
+        }
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("El maximo numero es: " + num1);
+            Console.WriteLine("El minimo numero es: " + num2);
+        } else
+        {
+            Console.WriteLine("El maximo numero es: " + num2);
+            Console.WriteLine("El minimo numero es: " + num1);
+        }
+    }
+    
+    Console.WriteLine("\nDesea realizar otro calculo? (0=No - 1=Si): ");
+    int.TryParse(Console.ReadLine(), out respuesta);
+
+} while (respuesta != 0);
+   
